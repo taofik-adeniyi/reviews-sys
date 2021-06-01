dfc63f2b4362dd9c5320b9b87c4b12e4c72c4755
taofik-adeniyi <bidemi64@gmail.com>
2021-06-01 13:01:04 +0100

    reviews set up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Review from './Review';
+function App() {
+  return <main>
+    <section className="container">
+      <div className="title">
+        <h2>customer reviews</h2>
+        <div className="underline" />
+      </div>
+      <Review />
+      </section></main>;
+}
+
+export default App;
